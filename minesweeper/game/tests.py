3da85188d2a296688d5bd535eff43ca7db99d511
Cynthia Monastirsky <cynthia.monastirsky@devecoop.com>
2020-04-22 12:55:35 -0300

    Add some API tests

--- minesweeper/game/tests.py
+++ minesweeper/game/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
+from django.conf import settings
 
-# Create your tests here.
+
+class APITestCase(TestCase):
+    def test_board_is_returned(self):
+        client = Client()
+        response = client.get('/game/board/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), settings.BOARD)
+
+    def test_game_ends_if_hitting_mine(self):
+        pass
