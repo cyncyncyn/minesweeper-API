2ba1d89be123e6c583661366ca9456bfd575be7e
Cynthia Monastirsky <cynthia.monastirsky@devecoop.com>
2020-04-22 13:03:50 -0300

    Add tests for click functionality

--- minesweeper/game/tests.py
+++ minesweeper/game/tests.py
@@ -11,4 +11,16 @@ class APITestCase(TestCase):
         self.assertEqual(response.json(), settings.BOARD)
 
     def test_game_ends_if_hitting_mine(self):
-        pass
+        client = Client()
+        response = client.post('/game/click/', {"x":1,"y":2})
+        self.assertEqual(response.status_code, 400)
+
+    def test_game_returns_200_if_no_mine_hit(self):
+        client = Client()
+        response = client.post('/game/click/', {"x":3,"y":4})
+        self.assertEqual(response.status_code, 200)
+
+    def test_game_returns_adjacent_squares_if_no_mine_hit(self):
+        client = Client()
+        response = client.post('/game/click/', {"x":3,"y":4})
+        self.assertEqual(response.status_code, 200)
