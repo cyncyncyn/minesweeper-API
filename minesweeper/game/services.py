e6ec9e2c11248c11caa2ff6667df103f278f4b57
Cynthia Monastirsky <cynthia.monastirsky@devecoop.com>
2020-04-23 12:22:50 -0300

    Fix function that calculates cells to be revealed

--- minesweeper/game/services.py
+++ minesweeper/game/services.py
@@ -22,17 +22,41 @@ def generate_board():
     return board
 
 
-def find_adjacents(board, x, y):
-    adjacents = []
-    rowLimit = len(board)
-    colLimit = len(board)
+def cell_has_mine(board, x, y):
+    return board[x][y]
 
-    for rowIndex in range(max(0, x -1), min(x + 1, rowLimit)):
-        for colIndex in range(max(0, y-1), min(y + 1, colLimit)):
-            if rowIndex != x and colIndex != y:
-                if board[rowIndex][colIndex]:
-                    return []
-                recursive_adjacents = find_adjacents(board, rowIndex, colIndex)
-                adjacents.extend(recursive_adjacents)
-                adjacents.append((rowIndex, colIndex))
-    return adjacents
+
+def surround_generator(x, y, max_row, max_col):
+    for i in range(max(0, x-1), min(x+2, max_row)):
+        for j in range(max(0, y-1), min(y+2, max_col)):
+            if x != i or y != j:
+                yield (i, j)
+
+
+def find_adjacents(board, x, y, visited=None):
+    visited = visited or []
+    visited.append((x, y))
+
+    rowLimit = len(board)
+    colLimit = len(board[0])
+
+    cells_to_reveal = []
+    has_mine = False
+    neighbours_has_mine = []
+    for i, j in surround_generator(x, y, rowLimit, colLimit):
+        has_mine = board[i][j]
+        neighbours_has_mine.append(has_mine)
+
+    if any(neighbours_has_mine):
+        return []
+    else:
+        for i, j in surround_generator(x, y, rowLimit, colLimit):
+            if (i, j) in visited:
+                continue
+
+            cells_to_reveal.append((i, j))
+            visited.append((i, j))
+            recursive_adjacents = find_adjacents(board, i, j, visited)
+            cells_to_reveal.extend(recursive_adjacents)
+
+    return cells_to_reveal
--- minesweeper/game/tests.py
+++ minesweeper/game/tests.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
-from django.test import Client
-from django.conf import settings
+from django.test import Client, TestCase
 
+from .services import BOARD_SIZE, find_adjacents
 
-from .services import BOARD_SIZE, find_adjacents # TODO: this should be a setting
 
 class APITestCase(TestCase):
+    # TODO: board is hardcoded for this demo version
+    # After this version we should use a random board and select
+    # a coord with each case (coord with adjacent mine, coord with mine,
+    #  coord with no adjacent mine)
     def test_board_is_returned(self):
         client = Client()
         response = client.get('/game/board/')
@@ -14,33 +16,148 @@ class APITestCase(TestCase):
 
     def test_game_ends_if_hitting_mine(self):
         client = Client()
-        response = client.post('/game/click/', '{"x":1,"y":2}'.encode('utf-8'), content_type="application/json")
+        response = client.post(
+            '/game/click/', '{"x":1,"y":2}'.encode('utf-8'),
+            content_type="application/json")
         self.assertEqual(response.status_code, 400)
 
     def test_game_returns_200_if_no_mine_hit(self):
         client = Client()
-        response = client.post('/game/click/', '{"x":3,"y":4}'.encode('utf-8'), content_type="application/json")
+        response = client.post(
+            '/game/click/', '{"x":3,"y":4}'.encode('utf-8'),
+            content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
-    def test_game_returns_adjacent_squares_if_no_mine_hit(self):
+    def test_returns_empty_adjacents_if_no_mine_hit_but_mine_is_neighbour(self):
         client = Client()
-        response = client.post('/game/click/', {"x":3,"y":4})
+        coord_with_adjacent_mine = '{"x": 3,"y": 4}'
+        response = client.post(
+            '/game/click/', coord_with_adjacent_mine.encode('utf-8'),
+            content_type="application/json")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
 
-class ServiceTestCase(TestCase):
-    def test_find_adjacents(self):
+    def test_returns_adjacents_if_no_mine_hit(self):
+        client = Client()
+        coord_with_adjacent_mine = '{"x": 7,"y": 7}'
+        response = client.post(
+            '/game/click/', coord_with_adjacent_mine.encode('utf-8'),
+            content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(), [
+                [6, 6], [5, 5], [5, 6], [5, 7], [6, 5], [6, 7], [7, 6], [7, 5]
+            ]
+        )
+
+
+class FindAdjacentsWithoutMinesTestCase(TestCase):
+
+    board = [
+            [False, False, False],
+            [False, False, False],
+            [False, False, False],
+    ]
+
+    def test_from_center(self):
+        adjacents = set(find_adjacents(self.board, 1, 1))
+        expected_adjacents = set(
+            [
+                (0, 0), (0, 1), (0, 2),
+                (1, 0),         (1, 2),
+                (2, 0), (2, 1), (2, 2)
+            ]
+        )
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+    def test_from_right_bottom(self):
+        adjacents = set(find_adjacents(self.board, 2, 2))
+        expected_adjacents = set(
+            [
+                (0, 0), (0, 1), (0, 2),
+                (1, 0), (1, 1), (1, 2),
+                (2, 0), (2, 1)
+            ]
+        )
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+    def test_from_right_top(self):
+        adjacents = set(find_adjacents(self.board, 0, 2))
+        expected_adjacents = set(
+            [
+                (0, 0), (0, 1),
+                (1, 0), (1, 1), (1, 2),
+                (2, 0), (2, 1), (2, 2)
+            ]
+        )
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+    def test_from_left_bottom(self):
+        adjacents = set(find_adjacents(self.board, 2, 0))
+        expected_adjacents = set(
+            [
+                (0, 0), (0, 1), (0, 2),
+                (1, 0), (1, 1), (1, 2),
+                        (2, 1), (2, 2)
+            ]
+        )
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+
+class FindAdjacentsWithMinesTestCase(TestCase):
+
+    def test_from_left_bottom_with_mine_in_center(self):
         board = [
             [False, False, False],
+            [False, True,  False],
+            [False, False, False],
+        ]
+
+        adjacents = set(find_adjacents(board, 2, 0))
+        expected_adjacents = set([])
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+    def test_from_left_bottom_with_mine_in_top_right(self):
+        board = [
+            [False, False, True],
             [False, False, False],
             [False, False, False],
         ]
 
-        adjacents = set(find_adjacents(board, 1,1))
-        expected_adjacents = set(
-            [(0,0), (0,1), (0,2),
-            (1,0),         (1,2),
-            (2,0), (2,1),  (2,2)
-            ]
-        )
+        adjacents = set(find_adjacents(board, 2, 0))
+        expected_adjacents = set([
+            (0, 0),
+            (0, 1),
+            (1, 0),
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (2, 2)
+        ])
+
+        self.assertEqual(adjacents, expected_adjacents)
+
+    def test_from_right_botom_with_mine_in_top_right(self):
+        board = [
+            [True,  False, False],
+            [False, False, False],
+            [False, False, False],
+        ]
+
+        adjacents = set(find_adjacents(board, 2, 2))
+        expected_adjacents = set([
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 1),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+        ])
 
         self.assertEqual(adjacents, expected_adjacents)
