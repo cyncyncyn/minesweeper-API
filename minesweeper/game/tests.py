2fc7d0e71bd6b02b33985503bd63701f3431d6db
Cynthia Monastirsky <cynthia.monastirsky@devecoop.com>
2020-05-04 13:23:30 -0300

    Fix tests

--- minesweeper/game/tests.py
+++ minesweeper/game/tests.py
@@ -1,56 +1,69 @@
 from django.test import Client, TestCase
 
-from .services import BOARD_SIZE, find_adjacents
-
-
-from django.conf import settings
+from game.services import find_adjacents
+from game.models import Board
 
 
 class APITestCase(TestCase):
-    BOARD_ID = 0
-    BOARD = settings.BOARDS[BOARD_ID]
-
     def get_cell_position(self, mine=True):
-        """ Returns first cell founds that matches mine param """
-        for i, row in enumerate(self.BOARD):
-            for j, col in enumerate(row):
-                if col == mine:
-                    return i, j
+        """ Return first cell founds that matches mine param """
+        amount_of_mines = 10 if mine else 0
+        board = Board(width=10, height=10, amount_of_mines=amount_of_mines)
+        board.generate_cells()
+        for cell in board.cell_set.all():
+            if mine and cell.is_mine:
+                return cell
+            if not mine and not cell.is_mine:
+                return cell
+
+    def setUp(self):
+        client = Client()
+        response = client.post('/game/board/')
+        response_json = response.json()
+        self.board_id = response_json["pk"]
 
     def test_board_is_returned(self):
         client = Client()
-        response = client.get('/game/board/')
+        response = client.get(f'/game/board/{self.board_id}/')
 
         self.assertEqual(response.status_code, 200)
 
         decoded_response = response.json()
-        self.assertEqual(decoded_response['boardSize'], BOARD_SIZE)
-        self.assertIn(decoded_response['boardId'], range(len(settings.BOARDS)))
+
+        self.assertEqual(decoded_response['pk'], self.board_id)
+        self.assertEqual(decoded_response['fields']['width'],
+                         Board.DEFAULT_BOARD_SIZE)
+        self.assertEqual(decoded_response['fields']['height'],
+                         Board.DEFAULT_BOARD_SIZE)
+        self.assertEqual(decoded_response['fields']['amount_of_mines'],
+                         Board.DEFAULT_MINES_AMOUNT)
+        self.assertEqual(decoded_response['fields']['status'], Board.PLAYING)
 
     def test_game_returns_400_if_hitting_mine(self):
-        x, y = self.get_cell_position(mine=True)
+        cell = self.get_cell_position(mine=True)
         client = Client()
         response = client.post(
-            '/game/click/', {"x": x, "y": y, "boardId": self.BOARD_ID},
+            '/game/click/',
+            {"x": cell.row, "y": cell.col, "boardId": cell.board.id},
             content_type="application/json")
         self.assertEqual(response.status_code, 400)
 
     def test_game_returns_200_if_no_mine_hit(self):
-        x, y = self.get_cell_position(mine=False)
+        cell = self.get_cell_position(mine=False)
         client = Client()
         response = client.post(
-            '/game/click/', {"x": x, "y": y, "boardId": self.BOARD_ID},
+            '/game/click/',
+            {"x": cell.row, "y": cell.col, "boardId": cell.board.id},
             content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
 
 class FindAdjacentsWithoutMinesTestCase(TestCase):
 
-    board = [
-            [False, False, False],
-            [False, False, False],
-            [False, False, False],
-    ]
+    def setUp(self):
+        board = Board(width=3, height=3, amount_of_mines=0)
+        board.generate_cells()
+        self.board = board
 
     def test_from_center(self):
         adjacents = set(find_adjacents(self.board, 1, 1))
@@ -104,11 +117,15 @@ class FindAdjacentsWithoutMinesTestCase(TestCase):
 class FindAdjacentsWithMinesTestCase(TestCase):
 
     def test_from_left_bottom_with_mine_in_center(self):
-        board = [
-            [False, False, False],
-            [False, True,  False],
-            [False, False, False],
-        ]
+        #    [False, False, False],
+        #    [False, True,  False],
+        #    [False, False, False],
+
+        board = Board(width=3, height=3, amount_of_mines=0)
+        board.generate_cells()
+        cell_1_1 = board.cell_set.get(row=1, col=1)
+        cell_1_1.is_mine = True
+        cell_1_1.save()
 
         adjacents = set(find_adjacents(board, 2, 0))
         expected_adjacents = set([])
@@ -116,11 +133,14 @@ class FindAdjacentsWithMinesTestCase(TestCase):
         self.assertEqual(adjacents, expected_adjacents)
 
     def test_from_left_bottom_with_mine_in_top_right(self):
-        board = [
-            [False, False, True],
-            [False, False, False],
-            [False, False, False],
-        ]
+        #    [False, False, True],
+        #    [False, False, False],
+        #    [False, False, False],
+        board = Board(width=3, height=3, amount_of_mines=0)
+        board.generate_cells()
+        cell_1_1 = board.cell_set.get(row=0, col=2)
+        cell_1_1.is_mine = True
+        cell_1_1.save()
 
         adjacents = set(find_adjacents(board, 2, 0))
         expected_adjacents = set([
@@ -136,11 +156,14 @@ class FindAdjacentsWithMinesTestCase(TestCase):
         self.assertEqual(adjacents, expected_adjacents)
 
     def test_from_right_botom_with_mine_in_top_right(self):
-        board = [
-            [True,  False, False],
-            [False, False, False],
-            [False, False, False],
-        ]
+        #    [True,  False, False],
+        #    [False, False, False],
+        #    [False, False, False],
+        board = Board(width=3, height=3, amount_of_mines=0)
+        board.generate_cells()
+        cell_0_0 = board.cell_set.get(row=0, col=0)
+        cell_0_0.is_mine = True
+        cell_0_0.save()
 
         adjacents = set(find_adjacents(board, 2, 2))
         expected_adjacents = set([
